3c284fbc4cde597f8852c3241dcd60c756f746e9
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-09-27 22:46:21 +0200

    Push 27/09/2022

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const app = express(); //Crée l'appli express
 
-app.use((req, res, next) => { //next permet de renvoyer à la prochaine fonction,l'execution du serveur
-    res.json({message: "hello motherfuck"});// réponse en JSON
-    next();
-});
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.setHeader('Content-Type','text/html');
     res.sendFile(path.join(__dirname, '/front/index.html'));
     app.use(express.static('front')) // permet de
+    next();
+});
+
+app.use((req, res) => {
+    console.log('Connexion works !!');// réponse dans la console
 })
 
 module.exports = app;
