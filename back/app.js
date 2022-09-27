bbf992e400c5603500b87f020b5a7c29a867d9c5
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-09-27 21:44:35 +0200

    test

--- back/app.js
+++ back/app.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({message: "hello motherfuck"})
-});
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type','text/html');
+    res.sendFile('../front/index.html');
+})
 
 module.exports = app;
+
+
+// // On appelle cela des middlesware
+// app.use((req, res, next) => {
+//     console.log('requete 1');// réponse dans la console
+//     next();
+// })
+
+// app.use((req, res, next) => {
+//     res.status(201);// réponse en code d'état 201
+//     next();
+// })
+// app.use((req, res, next) => { //next permet de renvoyer à la prochaine fonction,l'execution du serveur
+//     res.json({message: "hello motherfuck"});// réponse en JSON
+//     next();
+// });
+
+// app.use((req, res) => {
+//     console.log('mesage envoyer avec success !!');// réponse dans la console
+// })
